149e3ce911bf7de6b510d842256805bcfb553657
samojeyinka <samuelojeyinka@gmail.com>
2024-04-01 04:56:46 -1200

    authoriztion code to check logged in user and current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session([:user_id]);
+    end
+
+    def isloggedIn? 
+        !!current_user
+    end
 end
