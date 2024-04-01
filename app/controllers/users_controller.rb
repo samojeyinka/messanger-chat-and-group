ad12fd95168cfb095b29525947949354dada87f8
samojeyinka <samuelojeyinka@gmail.com>
2024-04-01 06:01:47 -1200

    added profie image upload during registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ end
 private
 
 def user_params
-    params.require(:user).permit(:username,:password,:password_confirmation)
+    params.require(:user).permit(:username,:password,:password_confirmation,:avatar)
 end
 
 def find_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :username, uniqueness:{ case_sensitive: false }, length:{minimum: 6, maximum:30}
     has_secure_password
-
+    has_one_attached :avatar
     
  
 
